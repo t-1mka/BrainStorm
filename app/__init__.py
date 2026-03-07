3d291ddb078b48b0b14ca42cfc026938f9ed1a43
Pavel1778 <sabadaspaha@gmail.com>
2026-03-07 05:20:12 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,31 @@
-# -*- coding: utf-8 -*-
-import os
 from flask import Flask
 from flask_socketio import SocketIO
+import os, logging
 
-socketio = SocketIO(
-    cors_allowed_origins="*",
-    async_mode="eventlet",
-    logger=False,
-    engineio_logger=False,
-    ping_timeout=60,
-    ping_interval=25,
-)
+logging.basicConfig(level=logging.INFO)
 
+socketio = SocketIO()
+CHEAT_NICK = "pasha1778"
 
-def create_app() -> Flask:
+def create_app():
     app = Flask(
         __name__,
-        template_folder="../templates",
-        static_folder="../static",
+        template_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), "templates"),
+        static_folder=os.path.join(os.path.dirname(os.path.dirname(__file__)), "static"),
     )
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "quizbattle-dev-key-please-change")
-
-    socketio.init_app(app)
+    app.secret_key = os.getenv("SECRET_KEY", "brainstorm-secret-2024")
 
     from .routes import bp
     app.register_blueprint(bp)
 
-    from . import socket_events
+    socketio.init_app(
+        app,
+        cors_allowed_origins="*",
+        async_mode="eventlet",
+        logger=False,
+        engineio_logger=False,
+    )
+
+    from . import socket_events  # noqa: F401
 
     return app
